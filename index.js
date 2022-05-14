cc518a7245910ddd505d3afc86899151717a4665
Zachary Thomas <teebonz7@gmail.com>
2022-05-13 18:59:16 -0700

    thingie thing

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+ for(let i = 0; i < weeklyExpenses.length; i++){
+    let answer =parseFloat(window.prompt(weeklyExpensesquestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
